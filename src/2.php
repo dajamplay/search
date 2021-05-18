3a6ec23a9eeabd84971bd6bf21278a7a89d766fb
dajamplay <ggyoug@gmail.cpm>
2021-05-18 11:48:45 +0300

    add 2.php > end sort

--- src/2.php
+++ src/2.php
@@ -1,2 +1,33 @@
 <?php
-    //сортировка пузырьком
+
+//сортировка пузырьком
+
+$items = [123, 22, 1, 0, 44, 23, 21, 111, 121];
+
+$isSort = false;
+
+echo '<pre>';
+print_r($items);
+echo '</pre>';
+
+while (!$isSort) {
+
+    $isSort = true;
+
+    for ($i = 0; $i < count($items) - 1; $i++) {
+
+        if ( $items[$i] > $items[$i + 1] )  {
+            list($items[$i], $items[($i + 1)]) = [$items[($i + 1)], $items[$i]];
+//            $tmp = $items[$i + 1];
+//            $items[$i + 1] = $items[$i];
+//            $items[$i] = $tmp;
+            $isSort = false;
+        }
+
+    }
+
+}
+
+echo '<pre>';
+print_r($items);
+echo '</pre>';
